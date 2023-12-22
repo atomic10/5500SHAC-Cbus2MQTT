4b2351663633a2f3bfc1fc4b0d40b81f58b1b3a2
atomic10 <ben.smith@pintek.org>
2023-12-22 15:01:00 +1100

    Update cbus2mqtt.lua
    Added functions for Trigger & Measurement applications

--- resident/cbus2mqtt.lua
+++ resident/cbus2mqtt.lua
@@ -4,10 +4,11 @@
 
 -- Install this script as a resident script with a sleep interval of 5 seconds
 
-mqtt_broker = 'YOUR_MQTT_HOST'
-mqtt_username = 'YOUR_MQTT_USERNAME'
-mqtt_password = 'YOUR_MQTT_PASSWORD'
-mqtt_clientid = 'cbus2mqtt'
+
+mqtt_broker = 'xxx.xxx.xxx.xxx'
+mqtt_username = 'xxxxxxxxxxxxx'
+mqtt_password = 'xxxxxxxxxxxxxxxxxxxxxxx'
+
 
 mqtt_read_topic = 'cbus/read/'
 
@@ -15,17 +16,17 @@ mqtt_read_topic = 'cbus/read/'
 mqtt = require("mosquitto")
 
 -- create new mqtt client
-client = mqtt.new(mqtt_clientid)
+client = mqtt.new()
 
-log("created MQTT client", client)
+--log("created MQTT client", client)
 
 -- C-Bus events to MQTT local listener
 server = require('socket').udp()
 server:settimeout(1)
-server:setsockname('127.0.0.1', 5432)
+server:setsockname('127.0.0.1', xxxx)
 
 client.ON_CONNECT = function()
-  log("MQTT connected - send")
+--  log("MQTT connected - send")
 end
 
 client:login_set(mqtt_username, mqtt_password)
@@ -34,8 +35,11 @@ client:loop_start()
 
 while true do
 	cmd = server:receive()
+  --log(cmd)
 	if cmd then
     parts = string.split(cmd, "/")
+    -- check application - if lighting / 56
+    if parts[2] == "56" then
     network = 254
     app = tonumber(parts[2])
     group = tonumber(parts[3])
@@ -43,5 +47,24 @@ while true do
   	state = (level ~= 0) and "ON" or "OFF"
     client:publish(mqtt_read_topic .. network .. "/" .. app .. "/" .. group .. "/state", state, 1, true)
     client:publish(mqtt_read_topic .. network .. "/" .. app .. "/" .. group .. "/level", level, 1, true)
-	end
+      
+    -- if application is measurement / 228
+    elseif parts[2] == "228" then
+      network = 254
+      app = tonumber(parts[2])
+      group = tonumber(parts[3])
+      subgroup = tonumber(parts[4])
+      status = tonumber(parts[5])
+      client:publish(mqtt_read_topic .. network .. "/" .. app .. "/" .. group .. "/" .. subgroup .. "/status", status, 1, true)
+  
+      -- if application is Trigger 
+    elseif parts[2] == "202" then
+      log("trigger", cmd)
+      network = 254
+      app = tonumber(parts[2])
+      group = tonumber(parts[3])
+      status = tonumber(parts[4])
+      client:publish(mqtt_read_topic .. network .. "/" .. app .. "/" .. group .. "/status", status, 1, true)     
+    end
+  end
 end
